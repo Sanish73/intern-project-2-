b8b8619743818c28d6c206e18ca3de3e54be0a83
Sanish Thapa <thapasanish73@gmail.com>
2022-08-28 13:03:24 +0545

    sjflas

--- School Pro/login.php
+++ School Pro/login.php
@@ -7,13 +7,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.8.3/font/bootstrap-icons.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.8.3/font/bootstrap-icons.min.css">
     <!-- End Bootstrap CSS -->
 
-        <!--Google Fonts-->
+    <!--Google Fonts-->
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
     <!--Custom Css-->
@@ -30,7 +28,7 @@
                     <div class="card-body">
                         <div class="offest-md-4">
                             <h1 class="text-center mt-4 mb-4 fw-bolder">Login</h1>
-                            <form class="form-login">
+                            <form class="form-login" method="POST">
                                 <!-- Email input -->
                                 <div class="form-outline mb-4">
                                     <label class="form-label" for="form2Example1">Email address</label>
@@ -41,15 +39,14 @@
                                     <label class="form-label " for="form2Example2">Password</label>
                                     <!-- Simple link -->
                                     <a href="#!" class="float-end text-primary">Forgot password?</a>
-                                    <input type="password" id="form2Example2" class="form-control" placeholder="Password"/>
+                                    <input type="password" id="form2Example2" class="form-control" placeholder="Password" />
                                 </div>
                                 <!-- 2 column grid layout for inline styling -->
                                 <div class="row mb-4">
                                     <div class="col d-flex">
                                         <!-- Checkbox -->
                                         <div class="form-check">
-                                            <input class="form-check-input" type="checkbox" value="" id="form2Example31"
-                                                checked />
+                                            <input class="form-check-input" type="checkbox" value="" id="form2Example31" checked />
                                             <label class="form-check-label" for="form2Example31"> Remember me </label>
                                         </div>
                                     </div>
@@ -57,8 +54,7 @@
 
                                 <!-- Submit button -->
                                 <div class="col-md-12">
-                                    <button type="button" class="col-12 btn btn-primary btn-block mb-4">Sign
-                                        in</button>
+                                   <input type="submit" class="col-12 btn btn-primary btn-block mb-4" value="submit" name="submit">
                                 </div>
                                 <p class="text-center"> Don't have account yet?<a href="register.php" class="text-primary"> Sign up</a></p>
                             </form>
@@ -71,11 +67,18 @@
 
 
 
+    <?php
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-        crossorigin="anonymous"></script>
+if(isset($_POST['submit'])){
+    echo ("hi");
+}
+
+    ?>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 </body>
 
